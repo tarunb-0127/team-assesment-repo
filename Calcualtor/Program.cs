fa851af2011e0ac04ea37f42e1338897b7daf369
tarunb-0127 <tarun.krithikabalaji@gmail.com>
2025-03-14 16:48:28 +0530

    Added a calculator project

---
+++ Calcualtor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ 
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+ 
+    public int Add(int a, int b, int c)
+    {
+        return this.Add(a, b) + c;
+    }
+}
+ 
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+ 
+        Console.WriteLine("Sum of 5 and 3: " + calc.Add(5, 3));
+        Console.WriteLine("Sum of 5, 3, and 2: " + calc.Add(5, 3, 2));
+    }
+}
